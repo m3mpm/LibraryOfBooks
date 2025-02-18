a5790faa3eb8f916d31be214858342bed648eaa5
Ruslan <m3mpm@users.noreply.github.com>
2025-02-18 13:48:57 +0700

    try to add tests

--- src/test/java/org/m3mpm/LibraryOfBooks/LibraryOfBooksApplicationTests.java
+++ src/test/java/org/m3mpm/LibraryOfBooks/LibraryOfBooksApplicationTests.java
@@ -1,13 +1,50 @@
 package org.m3mpm.LibraryOfBooks;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LibraryOfBooksApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+import org.junit.jupiter.api.Assertions;
+import org.m3mpm.LibraryOfBooks.model.Book;
+import org.m3mpm.LibraryOfBooks.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class LibraryOfBooksApplicationTests {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer
+            = new PostgreSQLContainer<>("postgres:15");
+
+    @DynamicPropertySource
+    static void registerPgProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSaveBook(){
+        Book book = new Book();
+        book.setTitle("Book Title");
+        book.setAuthor("Author");
+        book.setCreated_at(LocalDateTime.now());
+        book.setUpdated_at(LocalDateTime.now());
+        book.setPublishedDate(LocalDate.now());
+        Assertions.assertNotNull(bookRepository.save(book));
+    }
 
 }
